afe13659c0ba77d379dea55e7f49f5ccde6fb962
Lena Mangold <lena@mangold.org>
2021-03-04 10:57:03 -0500

    change elementType to type in test

--- tests/testthat/test-downloadDs.R
+++ tests/testthat/test-downloadDs.R
@@ -4,19 +4,22 @@ test_that("modal save function works", {
   user_name <- "test"
 
   library(hgchmagic)
-  el <- hgchmagic::hgch_bar_Cat(tibble(a = c("a","b")))
+  element <- hgchmagic::hgch_bar_Cat(tibble(a = c("a","b")))
   name <- "dsmodules test"
   slug <- dspins::create_slug(name)
-  elType <- "dsviz"
+  type <- "dsviz"
   html_link <- paste0("https://",bucket_id,".dskt.ch/",user_name,"/", slug, "/", slug,".html")
 
-  urls <- modalFunction_saveFile(element = el, elementType = elType, user_name = user_name, bucket_id = bucket_id,
+  urls <- modalFunction_saveFile(element = element, type = type, user_name = user_name, bucket_id = bucket_id,
                                  name = name)
 
   expect_equal(urls$link, paste0("https://datasketch.co/",user_name,"/", slug))
   expect_equal(urls$permalink, html_link)
   expect_equal(urls$iframe_embed, paste0("<iframe src=\"",html_link,"\" frameborder=0 width=\"100%\" height=\"400px\"></iframe>"))
 
-  expect_error(modalFunction_saveFile(elementType = elType, user_name = user_name), "Need element to save.")
+  expect_error(modalFunction_saveFile(type = type, user_name = user_name), "Need element to save.")
+  expect_error(modalFunction_saveFile(element = element, user_name = user_name), "Need type to save file.")
+  expect_error(modalFunction_saveFile(element = element, type = type), "Need user_name or org_name to save file.")
+  expect_error(modalFunction_saveFile(element = element, type = "blah", user_name = user_name), "Element must be of type 'fringe', 'dsviz', or 'drop'.")
 
 })
